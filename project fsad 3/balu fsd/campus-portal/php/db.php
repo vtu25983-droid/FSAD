c71854def5219ad883a46539df8332196b3375b2
vtu25983-droid <vtu25983@veltech.edu.in>
2026-05-06 14:29:59 +0530

    Delete project fsad 3/balu fsd/campus-portal directory

--- project fsad 3/balu fsd/campus-portal/php/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "campus_portal");
-if ($conn->connect_error) {
-    header("Content-Type: application/json");
-    echo json_encode(["success" => false, "error" => "DB Error: " . $conn->connect_error]);
-    exit();
-}
-$conn->set_charset("utf8mb4");
-?>
